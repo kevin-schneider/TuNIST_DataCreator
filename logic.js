40c35d422261e4ea8fb87a67ea400fa1f6577b32
Kevin Schneider <ke.schneider@stud.tu-darmstadt.de>
2018-02-22 16:03:54 +0100

    .

--- logic.js
+++ logic.js
@@ -160,16 +160,17 @@ function showDeleteScreen() {
 
 var nextDigit = function (save) {
     var canvasEmpty;
+    //ToDo rueckgaengig
     if (save) canvasEmpty = recognize();
-    // if (document.getElementById('preprocessing').checked == true) {
-    //     setTimeout(function () {
-    //         erase();
-    //     }, 3000);
-    // }
-    // else {
-    //     erase();
-    // }
-    erase();
+    if (document.getElementById('preprocessing').checked == true) {
+        setTimeout(function () {
+            erase();
+        }, 3000);
+    }
+    else {
+        erase();
+    }
+    //erase();
     if (!canvasEmpty) {
         updateProgress();
         createRandomDigit();
